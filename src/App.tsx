c560cc8aa8cfde31ee227f015cf9de28a1a43129
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-11-17 13:19:33 +0900

    変更

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          変更
+          変更2
         </p>
         <a
           className="App-link"
